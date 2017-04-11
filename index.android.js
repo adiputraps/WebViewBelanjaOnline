e58e375e464f2159b74e8208d0a0ff495d216bc9
adiputraps <adiputraps131313@gmail.com>
2017-04-11 09:34:03 +0800

    tahap 1

---
+++ index.android.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  Image,
+  ListView,
+  View
+} from 'react-native';
+import MainApp from './src/MainApp';
+AppRegistry.registerComponent('ch1p4', () => MainApp);
